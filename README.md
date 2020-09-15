188dabddd975890e019baaf5d7aa45cf7258e05d
榆泽 <zeyang.yzy@antfin.com>
2020-09-15 22:49:39 +0800

    1.【add】压缩脚本

---
+++ README.md
@@ -0,0 +1,7 @@
+## TinyPNG简版压缩脚本
+
+#### 使用方式
+
+1. 在 tinyPic.js 同级下新建 files 和 compressed 文件夹，将要压缩的图片放到 files 内，compress 会存放压缩后的图片
+2. 新建 .tinifyKey，将从 TinyPNG 申请的 Developer API 放到里面，注意 key 左右不要空格，换行等
+3. 执行 tinyPic.js，会帮你压缩到最小的体积。注意有些图可能会失真，记得检查一下🙂
---
+++ tinyPic.js
@@ -0,0 +1,54 @@
+const tinify = require('tinify');
+const fs = require('fs');
+
+tinify.key = fs.readFileSync('./.tinifyKey', 'utf8');
+tinify.validate(err => {
+  if (err) {
+    // 颜色日志输出：https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+    console.log('\x1b[31m', '#  - API key validate fail, please check it.');
+  } else {
+    const compressionsThisMonth = tinify.compressionCount;
+    const remainCount = 500 - compressionsThisMonth;
+    if (remainCount === 0) {
+      console.log('\x1b[31m', '#  - counts of compression are all used this month.');
+    }
+    console.log('\x1b[34m', `#  - this month you can use ${remainCount} times`);
+
+    fs.readdir('./files', (err, files) => {
+      if (err) {
+        console.log('\x1b[31m', '#  - read files error.');
+      } else if (files.length === 0) {
+        console.log('\x1b[31m', '#  - files is empty.');
+      } else {
+        console.log('\x1b[34m', '#  - files is compressing, please wait.');
+        files.forEach(fileName => {
+          fs.readFile(`./files/${fileName}`, (err, buf) => {
+            if (err) {
+              console.log('\x1b[31m', `#  - ${fileName} read error. ${err.message}`);
+            } else {
+              recursiveCompress(fileName, buf);
+            }
+          });
+        });
+      }
+    });
+  }
+});
+
+const recursiveCompress = (fileName, preBuf) => {
+  tinify.fromBuffer(preBuf).toBuffer((err, buf) => {
+    if (err) {
+      console.log('\x1b[31m', `#  - ${fileName} compress error. ${err.message}`);
+    } else if ((1 - buf.length/preBuf.length) > 0.001) {
+      recursiveCompress(fileName, buf);
+    } else {
+      fs.writeFile(`./compressed/${fileName}`, buf, err => {
+        if (err) {
+          console.log('\x1b[31m', `#  - ${fileName} write error. ${err.message}`);
+        } else {
+          console.log('\x1b[35m', `#  - ${fileName} compress done.`);
+        }
+      });
+    }
+  })
+}
